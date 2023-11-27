9b16dbf2811158eb21fd001bc834899628275d45
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-11-26 22:58:19 -0300

    first commit

---
+++ AppDigestoVillaNueva/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppdigestovillanuevaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppDigestoVillaNueva'
---
+++ AppDigestoVillaNueva/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import date
+
+# Create your models here.
+
+class Documento(models.Model):
+    fecha_creacion = models.DateField(default=date.today)
+    descripcion = models.TextField(max_length=2000)
+    archivo_adjunto = models.FileField(blank=True, null=True)
+    publicado = models.BooleanField(default=False)
+    eliminado = models.BooleanField(default=False)
+
+class Decreto(Documento):
+    numero_decreto = models.PositiveIntegerField(default=1)
+    anio = models.IntegerField(default=date.today().year)
+    fecha_publicacion = models.DateField(blank=True, null=True)
